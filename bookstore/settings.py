91f2ab855fa7149c7a9126fac2fbcf5bdefb5cfe
Josyway Paulo <josywaypaulo@gmail.com>
2025-03-24 09:56:05 -0300

    feat-pythonanywhere

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!9i!w6lu3b*l77_@ex8golecrx+$l$)@c!squmjz@1-bc6v27i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'Josyway.pythonanywhere.com']
 
 
 # Application definition
